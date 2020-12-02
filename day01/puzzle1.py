df6528c70983f6305719d506671d04ea534c0d98
techrabbit58 <dzb@pruefbit.de>
2020-12-02 09:33:50 +0100

    day01 puzzle1

---
+++ day01/puzzle1.py
@@ -0,0 +1,24 @@
+"""
+Day 01.
+Puzzle 1.
+Find, from a set of numbers, the two numbers that, when added,
+give 2020 as a result.
+Return the product of these two numbers.
+"""
+
+puzzle_input = 'day01.txt'
+
+
+def solution(puzzle_input: str):
+    with open(puzzle_input) as f:
+        numbers = set(int(x) for x in f.read().strip().split())
+    for p in sorted(numbers):
+        q = 2020 - p
+        if q in numbers:
+            result = p * q
+            break
+    return result
+
+
+if __name__ == '__main__':
+    print(solution(puzzle_input))
