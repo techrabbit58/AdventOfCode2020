bc5445accd845b5af720dfec88dff16e2b0d4d5a
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-03 15:22:43 +0100

    prepare for day04 etc.: prepare.py

---
+++ prepare.py
@@ -0,0 +1,65 @@
+"""
+Prepare directory and files for a new day of Advent Of Code 2020.
+"""
+import os
+
+if __name__ == '__main__':
+
+    day = int(input('Day? '))
+    if not 1 <= day <= 24:
+        print('Day must be a number from 1 to 24.')
+        print('Please, try again.')
+        exit()
+    
+    folder = f'day{day:02d}'
+    if os.path.exists(folder):
+        print(f'Folder "{folder}" already there.')
+    else:
+        os.mkdir(folder)
+    
+    os.chdir(folder)
+    
+    input_file = f'input{day:02d}.txt'
+    if os.path.exists(input_file):
+        print(f'Puzzle input file "{input_file}" already there.')
+    else:
+        with open(input_file, 'w'):
+            pass
+
+    puzzles = [f'puzzle{day:02d}-1.py', f'puzzle{day:02d}-2.py']
+
+    for n, puzzle in enumerate(puzzles, 1):
+
+        template = [
+                '"""',
+                f'Advent Of Code 2020. Day {day}. Puzzle {n}.',
+                '"""',
+                'import time',
+                '',
+                f"input_file = '{input_file}'",
+                '',
+                '',
+                'def parse(puzzle_input):',
+                '    puzzle = None',
+                '    with open(puzzle_input) as f:',
+                "        puzzle = f.read().strip().split('\\n')",
+                '    return puzzle',
+                '',
+                '',
+                'def solution(puzzle_input):',
+                '    puzzle = parse(puzzle_input)',
+                '    result = None',
+                '    return result',
+                '',
+                '',
+                "if __name__ == '__main__':",
+                '    start = time.perf_counter()',
+                '    print(solution(input_file), time.perf_counter() - start)',
+                ]
+
+        if os.path.exists(puzzle):
+            print(f'Python solution "{puzzle}" already there.')
+        else:
+            with open(puzzle, 'w') as f:
+                for line in template:
+                    print(line, file=f)
