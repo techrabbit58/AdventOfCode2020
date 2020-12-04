a5c02a1a923d42be5e20557261342bb00b31433f
techrabbit58 <dzbpruefbit.de>
2020-12-04 15:20:24 +0100

    day 5: pep8 fixes

--- day05/puzzle05.py
+++ day05/puzzle05.py
@@ -7,9 +7,9 @@ import time
 input_file = 'input05.txt'
 
 
-def parse(puzzle_input):
-    with open(puzzle_input) as f:
-        puzzle = f.read().strip().split('\n')
+def parse(fn):
+    with open(fn) as fh:
+        puzzle = fh.read().strip().split('\n')
     return puzzle
 
 
@@ -24,10 +24,10 @@ def part2(puzzle):
 
 
 if __name__ == '__main__':
-    puzzle = parse(input_file)
+    puzzle_input = parse(input_file)
 
     start = time.perf_counter()
-    print('part 1:', part1(puzzle), 'time', time.perf_counter() - start)
+    print('part 1:', part1(puzzle_input), 'time', time.perf_counter() - start)
 
     start = time.perf_counter()
-    print('part 2:', part2(puzzle), 'time', time.perf_counter() - start)
+    print('part 2:', part2(puzzle_input), 'time', time.perf_counter() - start)
