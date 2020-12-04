fb7380133627119addb553c7a6d1cfb292d36d1e
techrabbit58 <dzb@pruefbit.de>
2020-12-04 07:46:03 +0100

    solved: day 4 puzzle 2

--- day04/puzzle04.py
+++ day04/puzzle04.py
@@ -36,13 +36,36 @@ def part2(puzzle):
     for passport in puzzle:
         fields = analyze(passport)
         if not len(required_fields - set(fields.keys()) - {'cid'}):
+            if not 1920 <= int(fields['byr']) <= 2002:
+                continue
+            if not 2010 <= int(fields['iyr']) <= 2020:
+                continue
+            if not 2020 <= int(fields['eyr']) <= 2030:
+                continue
+            hgt_v, hgt_u = fields['hgt'][:-2], fields['hgt'][-2:]
+            if hgt_u == 'cm' and not 150 <= int(hgt_v) <= 193:
+                continue
+            elif hgt_u == 'in' and not 59 <= int(hgt_v) <= 76:
+                continue
+            elif hgt_u not in {'in', 'cm'}:
+                continue
+            hcl = fields['hcl']
+            if not (hcl[0] == '#' and len(hcl) == 7 and set(hcl[1:].lower()).issubset(set('0123456789abcdef'))):
+                continue
+            if fields['ecl'] not in set('amb blu brn gry grn hzl oth'.split()):
+                continue
+            pid = fields['pid']
+            if not (len(pid) == 9 and pid.isdecimal()):
+                continue
             result += 1
     return result
 
 
 if __name__ == '__main__':
     puzzle = parse(input_file)
+
     start = time.perf_counter()
     print(part1(puzzle), time.perf_counter() - start)
+
     start = time.perf_counter()
     print(part2(puzzle), time.perf_counter() - start)
