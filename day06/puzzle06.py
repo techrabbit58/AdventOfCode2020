8a9484a80162b829cc46553af772233837b257c7
techrabbit58 <dzb@pruefbit.de>
2020-12-05 08:05:23 +0100

    prepare for day 6

---
+++ day06/puzzle06.py
@@ -0,0 +1,33 @@
+"""
+Advent Of Code 2020.
+Day 6.
+"""
+import time
+
+input_file = 'input06.txt'
+
+
+def load(fn):
+    with open(fn) as fh:
+        puzzle = fh.read().strip().split('\n')
+    return puzzle
+
+
+def part1(puzzle):
+    result = puzzle
+    return result
+
+
+def part2(puzzle):
+    result = puzzle
+    return result
+
+
+if __name__ == '__main__':
+    puzzle_input = load(input_file)
+
+    start = time.perf_counter()
+    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+
+    start = time.perf_counter()
+    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
