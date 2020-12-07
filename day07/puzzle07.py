5706d0ddd506e3d6fd2f23a7051247d72ca7c0ab
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-07 11:31:59 +0100

    solved: day 7 parts 1 and 2

--- day07/puzzle07.py
+++ day07/puzzle07.py
@@ -6,8 +6,12 @@ import time
 import re
 from collections import defaultdict
 
-input_file = 'test07.txt'
-# input_file = 'input07.txt'
+input_file = 'input07.txt'
+
+my_bag = 'shiny gold'
+
+colors = re.compile(r'\d+ (\w+ \w+) bag[s]?[.,]')
+counts = re.compile(r'(\d+) (\w+ \w+) bag[s]?[.,]')
 
 
 def load(fn):
@@ -16,42 +20,51 @@ def load(fn):
     return puzzle
 
 
-my_bag = 'shiny gold'
-colors = re.compile(r'\d+ (\w+ \w+) bag[s]?[.,]')
-
-
 def parse(puzzle):
-    parents = defaultdict(set)
+    parents_ = defaultdict(set)
+    children_ = {}
     for sentence in puzzle:
         outer, _, other = sentence.partition(' bags contain ')
-        content = colors.findall(other)
+        content, rules = colors.findall(other), counts.findall(other)
         if content:
             for color in content:
-                parents[color].add(outer)
-    return parents
+                parents_[color].add(outer)
+            children_[outer] = [(int(n), c) for n, c in rules]
+    return parents_, children_
 
 
-def part1(puzzle):
-    parents = parse(puzzle)
-    containers = parents[my_bag]
-    for item in containers:
-        if containers.intersection(children):
-            containers.add(parent)
-    print(containers)
-    result = len(puzzle)
-    return result
+def find_all_parents(parents_graph, child, color_bag):
+    parents = parents_graph.get(child)
+    if not parents:
+        return color_bag
+    for parent in parents:
+        color_bag = find_all_parents(parents_graph, parent, color_bag).union({parent})
+    return color_bag
 
 
-def part2(puzzle):
-    result = puzzle
-    return result
+def count_all_children(children_graph, parent, counters):
+    children = children_graph.get(parent)
+    if not children:
+        return counters
+    for num, child in children:
+        counters += [num] + num * count_all_children(children_graph, child, [])
+    return counters
+
+
+def part1(parents_, my_bag_):
+    return len(find_all_parents(parents_, my_bag_, set()))
+
+
+def part2(children_, my_bag_):
+    return sum(count_all_children(children_, my_bag_, []))
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
+    parents, children = parse(puzzle_input)
 
     start = time.perf_counter()
-    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 1:', part1(parents, my_bag), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(children, my_bag), 'time', round(time.perf_counter() - start, 4))
