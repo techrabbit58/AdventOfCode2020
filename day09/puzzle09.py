3f37c19da0d990a4920be49a97523e380a223721
techrabbit58 <dzb@pruefbit.de>
2020-12-09 07:26:04 +0100

    solved: day 9

--- day09/puzzle09.py
+++ day09/puzzle09.py
@@ -5,29 +5,46 @@ Day 9.
 import time
 
 input_file = 'input09.txt'
+PREAMBLE = 25
 
 
 def load(fn):
     with open(fn) as fh:
-        puzzle = fh.read().strip().split('\n')
+        puzzle = [int(n) for n in fh.read().strip().split('\n')]
     return puzzle
 
 
 def part1(puzzle):
-    result = puzzle
-    return result
+    for p in range(PREAMBLE, len(puzzle)):
+        candidate = puzzle[p]
+        a = set(puzzle[p - PREAMBLE:p])
+        b = {candidate - x for x in a if x << 1 != candidate}
+        if not a.intersection(b):
+            return candidate, p
+    return None
 
 
-def part2(puzzle):
-    result = puzzle
-    return result
+def part2(puzzle, i):
+    weak_number = puzzle[i]
+    remaining = puzzle[:i]
+    for p in range(i):
+        for q in range(2, i - p):
+            subset = remaining[p:p + q]
+            s = sum(subset)
+            if s == weak_number:
+                subset.sort()
+                return subset[0] + subset[-1]
+            if s > weak_number:
+                break
+    return None
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
 
     start = time.perf_counter()
-    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    weak_number, index = part1(puzzle_input)
+    print('part 1:', weak_number, 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(puzzle_input, index), 'time', round(time.perf_counter() - start, 4))
