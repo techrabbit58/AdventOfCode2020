d0a680c378176a54bce26cf0fb99b72cf24898bf
techrabbit58 <dzb@pruefbit.de>
2020-12-10 06:50:57 +0100

    day 10 part 1 solved

--- day10/puzzle10.py
+++ day10/puzzle10.py
@@ -3,18 +3,35 @@ Advent Of Code 2020, Day 10.
 https://adventofcode.com/2020/day/10
 """
 import time
+from collections import defaultdict, Counter
 
 input_file = 'input10.txt'
 
 
 def load(fn):
     with open(fn) as fh:
-        puzzle = fh.read().strip().split('\n')
+        puzzle = [int(n) for n in fh.read().strip().split('\n')]
     return puzzle
 
 
 def part1(puzzle):
-    return puzzle
+    target_jolt = max(puzzle) + 3
+    input_rating = defaultdict(set)
+    for jolt in puzzle + [target_jolt]:
+        for d in range(1, 4):
+            if d <= jolt:
+                input_rating[jolt - d].add(jolt)
+    jolt = 0
+    deltas = Counter()
+    used = []
+    while jolt < target_jolt:
+        new_jolt = min(input_rating[jolt])
+        used.append(new_jolt)
+        deltas[new_jolt - jolt] += 1
+        jolt = new_jolt
+    print(sorted(puzzle))
+    print(used)
+    return deltas[1] * deltas[3]
 
 
 def part2(puzzle):
