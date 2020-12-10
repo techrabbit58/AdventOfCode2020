1e5c06255fbb62da3bb4315763a4f4f03075ac49
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-10 10:52:14 +0100

    refactored: day 10

--- day10/puzzle10.py
+++ day10/puzzle10.py
@@ -14,29 +14,10 @@ def load(fn):
     return puzzle
 
 
-def part1a(puzzle):
-    target_jolt = max(puzzle) + 3
-    input_rating = defaultdict(set)
-    for jolt in puzzle + [target_jolt]:
-        for d in range(1, 4):
-            if d <= jolt:
-                input_rating[jolt - d].add(jolt)
-    jolt = 0
+def part1(puzzle):
     deltas = Counter()
-    used = []
-    while jolt < target_jolt:
-        new_jolt = min(input_rating[jolt])
-        used.append(new_jolt)
-        deltas[new_jolt - jolt] += 1
-        jolt = new_jolt
-    return deltas[1] * deltas[3]
-
-
-def part1b(puzzle):
-    adapters = [0] + sorted(puzzle) + [max(puzzle) + 3]
-    deltas = Counter()
-    for p in range(len(adapters) - 1):
-        deltas[adapters[p + 1] - adapters[p]] += 1
+    for p in range(len(puzzle) - 1):
+        deltas[puzzle[p + 1] - puzzle[p]] += 1
     return deltas[1] * deltas[3]
 
 
@@ -57,23 +38,26 @@ def count_paths(jolt, target_jolt, ratings):
     return path_count
 
 
-def part2(puzzle):
-    target_jolt = max(puzzle) + 3
-    adapters = [0] + sorted(puzzle) + [target_jolt]
+def as_rating_graph(adapters):
     input_rating = defaultdict(list)
     for jolt in adapters:
         for d in range(1, 4):
             ir = jolt - d
             if ir in adapters:
                 input_rating[ir].append(jolt)
-    return count_paths(0, target_jolt, input_rating)
+    return input_rating
+
+
+def part2(puzzle):
+    return count_paths(0, puzzle[-1], as_rating_graph(puzzle))
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
+    jolt_list = [0] + sorted(puzzle_input) + [max(puzzle_input) + 3]
 
     start = time.perf_counter()
-    print('part 1:', part1b(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 1:', part1(jolt_list), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(jolt_list), 'time', round(time.perf_counter() - start, 4))
