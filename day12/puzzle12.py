842b8bab2b09a9eb8e4e7299bbc73c71cc3088dc
techrabbit58 <dzb@pruefbit.de>
2020-12-11 18:48:58 +0100

    prepare for day 12

---
+++ day12/puzzle12.py
@@ -0,0 +1,31 @@
+"""
+Advent Of Code 2020, Day 12.
+https://adventofcode.com/2020/day/12
+"""
+import time
+
+input_file = 'input12.txt'
+
+
+def load(fn):
+    with open(fn) as fh:
+        puzzle = fh.read().strip().split('\n')
+    return puzzle
+
+
+def part1(puzzle):
+    return puzzle
+
+
+def part2(puzzle):
+    return len(puzzle)
+
+
+if __name__ == '__main__':
+    puzzle_input = load(input_file)
+
+    start = time.perf_counter()
+    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+
+    start = time.perf_counter()
+    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
