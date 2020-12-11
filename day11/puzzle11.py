5e199ec5f2ac8dd44b3804b4eb3326957cdc50dd
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-11 13:15:32 +0100

    day 11 part 2: solved

--- day11/puzzle11.py
+++ day11/puzzle11.py
@@ -4,7 +4,7 @@ https://adventofcode.com/2020/day/11
 """
 import time
 
-input_file = 'test11.txt'
+input_file = 'input11.txt'
 
 
 def load(fn):
@@ -13,14 +13,8 @@ def load(fn):
     return puzzle
 
 
-def locate_seats(puzzle):
-    return {
-        (row, col) for row, line in enumerate(puzzle)
-        for col, position in enumerate(line)
-        if position == 'L'}, len(puzzle[0]), len(puzzle)
-
-
-def immediate_neighbourhood(location):
+def immediate_neighbourhood(location, seats):
+    assert seats is not None
     row, col = location
     yield row - 1, col - 1
     yield row - 1, col
@@ -32,14 +26,70 @@ def immediate_neighbourhood(location):
     yield row + 1, col + 1
 
 
-def simple_occupy(seat, seats, occupied):
-    num_neighbours = 0
-    for location in immediate_neighbourhood(seat):
-        if location in seats:
-            num_neighbours += 1 if location in occupied else 0
+def complex_neighbourhood(location, seats):
+    row, col = location
+    rows, cols  = max(seats, key=lambda s: s[0])[0] + 1, max(seats, key= lambda s: s[1])[1] + 1
+    # same row, to the east
+    for c in range(col + 1, cols):
+        if (row, c) in seats:
+            yield (row, c)
+            break
+    # same row, to the west
+    for c in range(col - 1, -1, -1):
+        if (row, c) in seats:
+            yield (row, c)
+            break
+    # same column, to the north
+    for r in range(row - 1, -1, -1):
+        if (r, col) in seats:
+            yield (r, col)
+            break
+    # same column, to the south
+    for r in range(row + 1, rows):
+        if (r, col) in seats:
+            yield (r, col)
+            break
+    # south western
+    for rc in zip(range(row + 1, rows), range(col - 1, -1, -1)):
+        if rc in seats:
+            yield rc
+            break
+    # south eastern
+    for rc in zip(range(row + 1, rows), range(col + 1, cols)):
+        if rc in seats:
+            yield rc
+            break
+    # north eastern
+    for rc in zip(range(row - 1, -1, -1), range(col + 1, cols)):
+        if rc in seats:
+            yield rc
+            break
+    # north western
+    for rc in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
+        if rc in seats:
+            yield rc
+            break
+
+
+def locate_seats(puzzle):
+    return {(row, col): set()
+        for row, line in enumerate(puzzle)
+        for col, position in enumerate(line)
+        if position != '.'}
+
+
+def assign_neighbourhood(seats, neighbourhood):
+    for seat, neighbours in seats.items():
+        for candidate in neighbourhood(seat, seats):
+            if candidate in seats:
+                neighbours.add(candidate)
+
+
+def occupy(seat, neighbours, occupied, *, threshold):
+    num_neighbours = len(neighbours.intersection(occupied))
     if num_neighbours == 0:
         return True
-    if num_neighbours >= 4:
+    if num_neighbours >= threshold:
         return False
     return seat in occupied
 
@@ -48,21 +98,26 @@ def part1(seats):
     occupied, last_occupied = {}, None
     while occupied != last_occupied:
         last_occupied = occupied
-        occupied = {seat for seat in seats if simple_occupy(seat, seats, occupied)}
+        occupied = {seat for seat in seats if occupy(seat, seats[seat], occupied, threshold=4)}
     return len(occupied)
 
 
-def part2(seats, rows, columns):
+def part2(seats):
     occupied, last_occupied = {}, None
-    return len(occupied), rows, columns
+    while occupied != last_occupied:
+        last_occupied = occupied
+        occupied = {seat for seat in seats if occupy(seat, seats[seat], occupied, threshold=5)}
+    return len(occupied)
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
-    seat_locations, rows, columns = locate_seats(puzzle_input)
+    seat_locations = locate_seats(puzzle_input)
 
     start = time.perf_counter()
+    assign_neighbourhood(seat_locations, immediate_neighbourhood)
     print('part 1:', part1(seat_locations), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(seat_locations, rows, columns), 'time', round(time.perf_counter() - start, 4))
+    assign_neighbourhood(seat_locations, complex_neighbourhood)
+    print('part 2:', part2(seat_locations), 'time', round(time.perf_counter() - start, 4))
