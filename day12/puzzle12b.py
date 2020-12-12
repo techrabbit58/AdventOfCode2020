5c03e8f9c1e17df4f37713257235544882126666
techrabbit58 <dzb@pruefbit.de>
2020-12-12 09:54:38 +0100

    day 12: alternate solution, based on complex numbers

--- day12/puzzle12b.py
+++ day12/puzzle12b.py
@@ -1,29 +1,49 @@
 """
-Advent Of Code 2020, Day 13.
-https://adventofcode.com/2020/day/13
-Instead of 2D vectors, complex numbers can be used.
+Advent Of Code 2020, Day 12.
+https://adventofcode.com/2020/day/12
+Instead of 2D vectors, complex numbers can be used, which all in all is faster
+and gives shorter code. This alternate solution is a tribute to GitHub user "fuglede":
+https://github.com/fuglede/adventofcode/tree/master/2020
 """
 import time
 
-input_file = '../day13/input13.txt'
+input_file = '../day12/input12.txt'
 
 
 def load(fn):
     with open(fn) as fh:
-        puzzle = [(ins[0], int(ins[1:])) for ins in fh.read().strip().split('\n')
+        puzzle = [(ins[0], int(ins[1:])) for ins in fh.read().strip().split('\n')]
     return puzzle
+
+
+direction = {'N': 1j, 'E': 1, 'W': -1, 'S': -1j}
+rotation = {'L': 1j, 'R': -1j}
 
 
 def part1(puzzle):
-    position = 0
+    ship = 0
     heading = 1
     for action, value in puzzle:
-        pass
-    return puzzle
+        if action in direction:
+            ship += direction[action] * value
+        elif action in rotation:
+            heading *= rotation[action] ** (value / 90)
+        else:
+            ship += value * heading
+    return int(abs(ship.real) + abs(ship.imag))
 
 
 def part2(puzzle):
-    return len(puzzle)
+    ship = 0
+    waypoint = 10 + 1j
+    for action, value in puzzle:
+        if action in direction:
+            waypoint += direction[action] * value
+        elif action in rotation:
+            waypoint *= rotation[action] ** (value / 90)
+        else:
+            ship += value * waypoint
+    return int(abs(ship.real) + abs(ship.imag))
 
 
 if __name__ == '__main__':
