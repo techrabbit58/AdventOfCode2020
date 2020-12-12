18c78a51c3b4275362cf6b317fefd712330d205e
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-12 18:50:38 +0100

    prepare for day 13

--- day13/puzzle13.py
+++ day13/puzzle13.py
@@ -9,8 +9,11 @@ input_file = 'input13.txt'
 
 def load(fn):
     with open(fn) as fh:
-        puzzle = fh.read().strip().split('\n')
-    return puzzle
+        return fh.read().strip()
+
+
+def prepare(raw_input):
+    return [line for line in raw_input.split('\n')]
 
 
 def part1(puzzle):
@@ -23,9 +26,10 @@ def part2(puzzle):
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
+    input_records = prepare(puzzle_input)
 
     start = time.perf_counter()
-    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 1:', part1(input_records), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(input_records), 'time', round(time.perf_counter() - start, 4))
--- prepare.py
+++ prepare.py
@@ -10,15 +10,15 @@ if __name__ == '__main__':
         print('Day must be a number from 1 to 24.')
         print('Please, try again.')
         exit()
-    
+
     folder = f'day{day:02d}'
     if os.path.exists(folder):
         print(f'Folder "{folder}" already there.')
     else:
         os.mkdir(folder)
-    
+
     os.chdir(folder)
-    
+
     input_file = f'input{day:02d}.txt'
     if os.path.exists(input_file):
         print(f'Puzzle input file "{input_file}" already there.')
@@ -31,38 +31,44 @@ if __name__ == '__main__':
     for n, puzzle in enumerate(puzzles, 1):
 
         template = [
-                '"""',
-                f'Advent Of Code 2020, Day {day}.',
-                f'https://adventofcode.com/2020/day/{day}',
-                '"""',
-                'import time',
-                '',
-                f"input_file = '{input_file}'",
-                '',
-                '',
-                'def load(fn):',
-                '    with open(fn) as fh:',
-                "        puzzle = fh.read().strip().split('\\n')",
-                '    return puzzle',
-                '',
-                '',
-                'def part1(puzzle):',
-                '    return puzzle',
-                '',
-                '',
-                'def part2(puzzle):',
-                '    return len(puzzle)',
-                '',
-                '',
-                "if __name__ == '__main__':",
-                '    puzzle_input = load(input_file)',
-                '',
-                '    start = time.perf_counter()',
-                "    print('part 1:', part1(puzzle_input), 'time', round(time.perf_counter() - start, 4))",
-                '',
-                '    start = time.perf_counter()',
-                "    print('part 2:', part2(puzzle_input), 'time', round(time.perf_counter() - start, 4))",
-                ]
+            '"""',
+            f'Advent Of Code 2020, Day {day}.',
+            f'https://adventofcode.com/2020/day/{day}',
+            '"""',
+            'import time',
+            '',
+            f"input_file = '{input_file}'",
+            '',
+            '',
+            'def load(fn):',
+            '    with open(fn) as fh:',
+            "        return fh.read().strip()",
+            '',
+            '',
+            'def prepare(raw_input):',
+            "    return [line for line in raw_input.split('\\n')]",
+            '',
+            '',
+            'def part1(puzzle):',
+            '    return puzzle',
+            '',
+            '',
+            'def part2(puzzle):',
+            '    return len(puzzle)',
+            '',
+            '',
+            "if __name__ == '__main__':",
+            '    puzzle_input = load(input_file)',
+            '    input_records = prepare(puzzle_input)',
+            '',
+            '    start = time.perf_counter()',
+            "    print('part 1:', part1(input_records), 'time', round(time.perf_counter() - "
+            "start, 4))",
+            '',
+            '    start = time.perf_counter()',
+            "    print('part 2:', part2(input_records), 'time', round(time.perf_counter() - "
+            "start, 4))",
+        ]
 
         if os.path.exists(puzzle):
             print(f'Python solution "{puzzle}" already there.')
