b6dc1b1cd39730ff95fe32e6582517533156084d
techrabbit58 <dzb@pruefbit.de>
2020-12-12 08:32:13 +0100

    solved: day 12

--- day12/puzzle12.py
+++ day12/puzzle12.py
@@ -13,12 +13,55 @@ def load(fn):
     return puzzle
 
 
+def navigate(position, heading, instruction):
+    action, value = instruction[0].upper(), int(instruction[1:])
+    east, north = position
+    d_east, d_north = heading
+    if action == 'N':
+        north += value
+    elif action == 'S':
+        north -= value
+    elif action == 'E':
+        east += value
+    elif action == 'W':
+        east -= value
+    elif action == 'F':
+        east += value * d_east
+        north += value * d_north
+    elif instruction in {'L90', 'R270'}:
+        d_east, d_north = -d_north, d_east
+    elif instruction in {'L180', 'R180'}:
+        d_east, d_north = -d_east, -d_north
+    elif instruction in {'L270', 'R90'}:
+        d_east, d_north = d_north, -d_east
+    else:
+        raise ValueError(f'nad action "{action}"')
+    return (east, north), (d_east, d_north)
+
+
 def part1(puzzle):
-    return puzzle
+    ship = 0, 0
+    heading = 1, 0
+    for instruction in puzzle:
+        ship, heading = navigate(ship, heading, instruction)
+    east, north = ship
+    return abs(east) + abs(north)
 
 
 def part2(puzzle):
-    return len(puzzle)
+    ship = (0, 0)
+    waypoint = (10, 1)
+    print(ship, waypoint)
+    for instruction in puzzle:
+        if instruction.startswith('F'):
+            ship, _ = navigate(ship, waypoint, instruction)
+        elif instruction[0] in 'NEWS':
+            waypoint, _ = navigate(waypoint, waypoint, instruction)
+        else:
+            _, waypoint = navigate(waypoint, waypoint, instruction)
+        print(ship, waypoint)
+    east, north = ship
+    return abs(east) + abs(north)
 
 
 if __name__ == '__main__':
