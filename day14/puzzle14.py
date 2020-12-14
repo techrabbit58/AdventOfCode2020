3ae2c012afb2acd557108903b9a7c09b446a11d5
techrabbit58 <dzb@pruefbit.de>
2020-12-14 08:03:55 +0100

    day 14: part 1 solved, part 2 w.i.p.

--- day14/puzzle14.py
+++ day14/puzzle14.py
@@ -3,6 +3,10 @@ Advent Of Code 2020, Day 14.
 https://adventofcode.com/2020/day/14
 """
 import time
+import re
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import NamedTuple
 
 input_file = 'input14.txt'
 
@@ -13,19 +17,69 @@ def load(fn):
 
 
 def prepare(raw_input):
-    return [line for line in raw_input.split('\n')]
+    return raw_input.split('\n')
+
+
+mask_decode = re.compile(r'mask = ([01X]{36})')
+mem_decode = re.compile(r'mem\[(\d+)\] = (\d+)')
+
+MEM = 'mem'
+MASK = 'mask'
+
+
+@dataclass
+class Computer:
+    mask = {}
+    mem = {}
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(mask={self.mask}, mem={self.mem})'
+
+
+def decode(line):
+    if line.startswith('mem'):
+        args = mem_decode.match(line)
+        return MEM, (int(args[1]), int(args[2]))
+    elif line.startswith('mask'):
+        args = mask_decode.match(line)
+        mask = {2 ** (35 - n): int(bit) for n, bit in enumerate(args[1]) if bit in '01'}
+        return MASK, mask
+    else:
+        raise ValueError(f'ERROR - unknown instruction: "{line}".')
 
 
 def part1(puzzle):
-    return puzzle
+    computer = Computer()
+    for line in puzzle:
+        operation, operands = decode(line)
+        if operation == MASK:
+            computer.mask = operands
+        else:
+            address, arg = operands
+            for bit, value in computer.mask.items():
+                arg = arg | bit if value else arg & ~bit
+            computer.mem[address] = arg
+    return sum(computer.mem.values())
 
 
 def part2(puzzle):
-    return len(puzzle)
+    computer = Computer()
+    for line in puzzle:
+        operation, operands = decode(line)
+        print(operation, operands)
+        if operation == MASK:
+            computer.mask = operands
+        else:
+            pass
+    return None
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
+    puzzle_input = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""
     input_records = prepare(puzzle_input)
 
     start = time.perf_counter()
