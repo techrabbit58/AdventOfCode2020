878e62dfb10d3f3884905b19fd6da6f63f41c56b
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-14 13:12:33 +0100

    prepare for day 15

---
+++ day15/puzzle15.py
@@ -0,0 +1,35 @@
+"""
+Advent Of Code 2020, Day 15.
+https://adventofcode.com/2020/day/15
+"""
+import time
+
+input_file = 'input15.txt'
+
+
+def load(fn):
+    with open(fn) as fh:
+        return fh.read().strip()
+
+
+def prepare(raw_input):
+    return [line for line in raw_input.split('\n')]
+
+
+def part1(puzzle):
+    return puzzle
+
+
+def part2(puzzle):
+    return len(puzzle)
+
+
+if __name__ == '__main__':
+    puzzle_input = load(input_file)
+    input_records = prepare(puzzle_input)
+
+    start = time.perf_counter()
+    print('part 1:', part1(input_records), 'time', round(time.perf_counter() - start, 4))
+
+    start = time.perf_counter()
+    print('part 2:', part2(input_records), 'time', round(time.perf_counter() - start, 4))
