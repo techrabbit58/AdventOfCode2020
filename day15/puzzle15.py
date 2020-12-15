63b86015b83080c2dae4d06088e5e2a898929298
techrabbit58 <dzb@pruefbit.de>
2020-12-15 18:07:59 +0100

    day 15: check out some other solutions

--- day15/puzzle15.py
+++ day15/puzzle15.py
@@ -1,6 +1,7 @@
 """
 Advent Of Code 2020, Day 15.
 https://adventofcode.com/2020/day/15
+This puzzle has some relation to the so called "Van Eck Sequence".
 """
 import time
 
@@ -16,39 +17,72 @@ def prepare():
 
 def solution(puzzle, stop=2020):
     """
-    The solution is reasonable fast for low stops, but takes long for
-    high stops. I optimized my working solution (which took ages),
-    following somme hints and suggestions of Bradley Sward.
-    Bradley Sward is currently an Associate Professor at the
-    College of DuPage in suburban Chicago, Illinois.
-    The final solution takes more than 25 seconds on my
-    Raspberry Pi 400.
+    The solution is reasonably fast for low stops, but takes long for
+    high stops. I optimized my already working solution (which unfortunately
+    took ages of runtime for part 2), following somme hints and suggestions of
+    Bradley Sward. Bradley Sward is currently an Associate Professor at the
+    College of DuPage in suburban Chicago, Illinois. The final result takes
+    about 30 seconds on a Raspberry Pi 400, for part 2.
     """
-    spoken = {}
-    offset = 1
-    for number in puzzle:
-        spoken[number] = offset
-        offset += 1
+    spoken = {number: offset for offset, number in enumerate(puzzle, 1)}
     this = 0
     for turn in range(len(puzzle) + 1, stop):
         if this in spoken:
-            offset = turn - spoken[this]
-            spoken[this] = turn
-            this = offset
+            spoken[this], this = turn, turn - spoken[this]
         else:
-            spoken[this] = turn
-            this = 0
+            spoken[this], this = turn, 0
         if turn % 100000 == 99999:
-            print(f'{turn/stop:4.2f}\r', end='')
+            print(f'{turn / stop:4.2f}')
     return this
 
+
+def solution_b(puzzle, stop=2020):
+    """
+    This next solution is even faster, about 25%, thanks to Gravitar64.
+    https://github.com/Gravitar64/Advent-of-Code-2020
+    Counting the turns from len(puzzle) instead of len(puzzle) + 1 makes
+    everything so easy and nice!
+    """
+    spoken = {last: turn for turn, last in enumerate(puzzle, 1)}
+    last = puzzle[-1]
+    for turn in range(len(puzzle), stop):
+        recent = spoken.get(last, turn)
+        spoken[last] = turn
+        last = turn - recent
+    return last
+
+
+def recitation(puzzle):
+    from itertools import count
+
+    def iterate():
+        spoken = {last: turn for turn, last in enumerate(puzzle, 1)}
+        last = puzzle[-1]
+        for turn in count(len(puzzle)):
+            recent = spoken.get(last, turn)
+            spoken[last] = turn
+            last = turn - recent
+            yield turn + 1, last
+
+    return iterate
+
+
+def solution_c(puzzle, stop=2020):
+    """
+    The "lazy" approach is relatively slow. Didn't expect that.
+    Possibly because of the explicit stop condition in the main loop?
+    """
+    memory_game = recitation(puzzle)
+    for turn, last in memory_game():
+        if turn == stop:
+            return last
 
 
 if __name__ == '__main__':
     input_records = prepare()
 
     start = time.perf_counter()
-    print('part 1:', solution(input_records), 'time', round(time.perf_counter() - start, 4))
+    print('part 1:', solution_b(input_records), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', solution(input_records, stop=30000000), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', solution_b(input_records, stop=30000000), 'time', round(time.perf_counter() - start, 4))
