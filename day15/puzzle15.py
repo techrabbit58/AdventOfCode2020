83a2939997cdb6d134d98f98e4e6d870b407fd0a
techrabbit58 <dzb@pruefbit.de>
2020-12-15 08:05:40 +0100

    solved: day 15 part 1

--- day15/puzzle15.py
+++ day15/puzzle15.py
@@ -3,33 +3,42 @@ Advent Of Code 2020, Day 15.
 https://adventofcode.com/2020/day/15
 """
 import time
-
-input_file = 'input15.txt'
-
-
-def load(fn):
-    with open(fn) as fh:
-        return fh.read().strip()
+from collections import defaultdict
 
 
-def prepare(raw_input):
-    return [line for line in raw_input.split('\n')]
+def prepare():
+    # return [1, 0, 15, 2, 10, 13]
+    return [0, 3, 6]
 
 
-def part1(puzzle):
-    return puzzle
+def part1(puzzle, stop=2020):
+    spoken = defaultdict(list)
+    sequence = puzzle[:]
+    last = None
+    for turn, number in enumerate(puzzle, 1):
+        spoken[number].append(turn)
+        last = number
+    for turn in range(len(puzzle) + 1, stop + 1):
+        if len(spoken[last]) <= 1:
+            this = 0
+        else:
+            this = spoken[last][-1] - spoken[last][-2]
+        spoken[this].append(turn)
+        last = this
+        sequence.append(last)
+    return last, sequence
 
 
-def part2(puzzle):
-    return len(puzzle)
+def part2(sequence, stop=30000000):
+    return stop, sequence
 
 
 if __name__ == '__main__':
-    puzzle_input = load(input_file)
-    input_records = prepare(puzzle_input)
+    input_records = prepare()
 
     start = time.perf_counter()
-    print('part 1:', part1(input_records), 'time', round(time.perf_counter() - start, 4))
+    last, sequence = part1(input_records)
+    print('part 1:', last, 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(input_records), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(sequence), 'time', round(time.perf_counter() - start, 4))
