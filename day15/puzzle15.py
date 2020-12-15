bd0ca8dfe344e4a752738878cc6d1c2df498eec4
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2020-12-15 13:04:59 +0100

    day 15 part 2 solved

--- day15/puzzle15.py
+++ day15/puzzle15.py
@@ -3,52 +3,48 @@ Advent Of Code 2020, Day 15.
 https://adventofcode.com/2020/day/15
 """
 import time
-from collections import defaultdict
 
 
 def prepare():
     """
+    Given 0,3,6, the 2020th number spoken is 436.
     Given 0,3,6, the 30000000th number spoken is 175594.
-    Given 1,3,2, the 30000000th number spoken is 2578.
-    Given 2,1,3, the 30000000th number spoken is 3544142.
-    Given 1,2,3, the 30000000th number spoken is 261214.
-    Given 2,3,1, the 30000000th number spoken is 6895259.
-    Given 3,2,1, the 30000000th number spoken is 18.
-    Given 3,1,2, the 30000000th number spoken is 362.
     """
-    # return [1, 0, 15, 2, 10, 13]
-    return [0, 3, 6]
+    return [1, 0, 15, 2, 10, 13]
+    # return [0, 3, 6]
 
 
-def part1(puzzle, stop=2020):
-    spoken = defaultdict(list)
-    sequence = puzzle[:]
-    last = None
-    for turn, number in enumerate(puzzle, 1):
-        spoken[number].append(turn)
-        last = number
-    for turn in range(len(puzzle) + 1, stop + 1):
-        if len(spoken[last]) <= 1:
-            this = 0
+def solution(puzzle, stop=2020):
+    """
+    The solution is reasonable fast for low stops, but takes long for
+    high stops. I optimized my working solution (which took ages),
+    following somme hints and suggestions of Bradley Sward.
+    Bradley Sward is currently an Associate Professor at the
+    College of DuPage in suburban Chicago, Illinois.
+    """
+    spoken = {}
+    offset = 1
+    for number in puzzle:
+        spoken[number] = offset
+        offset += 1
+    this = 0
+    for turn in range(len(puzzle) + 1, stop):
+        if this in spoken:
+            offset = turn - spoken[this]
+            spoken[this] = turn
+            this = offset
         else:
-            this = spoken[last][-1] - spoken[last][-2]
-        spoken[this].append(turn)
-        last = this
-        sequence.append(last)
-    return last, sequence
+            spoken[this] = turn
+            this = 0
+    return this
 
-
-def part2(puzzle, sequence, stop=30000000):
-    print(puzzle)
-    return stop, sequence
 
 
 if __name__ == '__main__':
     input_records = prepare()
 
     start = time.perf_counter()
-    last, sequence = part1(input_records)
-    print('part 1:', last, 'time', round(time.perf_counter() - start, 4))
+    print('part 1:', solution(input_records), 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(input_records, sequence), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', solution(input_records, stop=30000000), 'time', round(time.perf_counter() - start, 4))
