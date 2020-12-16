e8a003752a70a24dcb76147d6869af42b725044d
techrabbit58 <dzb@pruefbit.de>
2020-12-16 07:56:33 +0100

    day 16: part 1 solved, part 2 w.i.p.

--- day16/puzzle16.py
+++ day16/puzzle16.py
@@ -3,8 +3,11 @@ Advent Of Code 2020, Day 16.
 https://adventofcode.com/2020/day/16
 """
 import time
+import re
 
-input_file = 'input16.txt'
+# input_file = 'test16a.txt'
+input_file = 'test16b.txt'
+# input_file = 'input16.txt'
 
 
 def load(fn):
@@ -13,23 +16,85 @@ def load(fn):
 
 
 def prepare(raw_input):
-    return [line for line in raw_input.split('\n')]
+    """
+    Input is organized in three chunks: (1) rules, (2) my ticket, (3) tickets nearby.
+    Must be prepared following different rules.
+        (1) label ':' lower 'or' upper ',' lower 'or' upper
+        (2) header line "your ticket:" followed by one line of comma separated integers
+        (3) same as (2), but many lines of comma separated integers
+    The CSV lines have always as many fields as there are rules in (1).
+    """
+    rules, my_ticket, tickets_nearby = [line for line in raw_input.split('\n\n')]
+    return rules, my_ticket, tickets_nearby
 
 
-def part1(puzzle):
-    return puzzle
+rule_split = re.compile(r'([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)')
 
 
-def part2(puzzle):
-    return len(puzzle)
+def parse_rules(raw):
+    rules = {}
+    for group in rule_split.findall(raw):
+        label, a, b, c, d = group
+        rules[label] = {i for i in range(int(a), int(b) + 1)} | {i for i in range(int(c), int(d) + 1)}
+    return rules
+
+
+def parse_tickets(raw):
+    tickets = []
+    for line in raw.split('\n'):
+        if line in {'your ticket:', 'nearby tickets:'}:
+            continue
+        tickets.append([int(i) for i in line.split(',')])
+    return tickets
+
+
+def part1(rules, tickets):
+    valid_values = set()
+    good_tickets = []
+    for values in rules.values():
+        valid_values.update(values)
+    errors = 0
+    for ticket in tickets:
+        errors_so_far = errors
+        for field in ticket:
+            if field not in valid_values:
+                errors += field
+        if errors - errors_so_far == 0:
+            good_tickets.append(ticket)
+    return good_tickets, errors
+
+
+def transpose(matrix):
+    result = [[] for _ in range(len(matrix[0]))]
+    for y, row in enumerate(matrix):
+        for x, col in enumerate(row):
+            result[x].append(matrix[y][x])
+    return result
+
+
+def part2(rules, mine, others):
+    """NOT SOLVED!"""
+    tickets = others.copy() + [mine]
+    fields = transpose(tickets)
+    num_fields = len(rules)
+    candidates = {n: set() for n in range(num_fields)}
+    for n, field in enumerate(fields):
+        for label, rule in rules.items():
+            if rule.intersection(field):
+                candidates[n].add(label)
+    return candidates
 
 
 if __name__ == '__main__':
     puzzle_input = load(input_file)
-    input_records = prepare(puzzle_input)
+    raw_rules, raw_my_ticket, raw_tickets_nearby = prepare(puzzle_input)
+    rule_base = parse_rules(raw_rules)
+    my_ticket = parse_tickets(raw_my_ticket)[0]
+    nearby_tickets = parse_tickets(raw_tickets_nearby)
 
     start = time.perf_counter()
-    print('part 1:', part1(input_records), 'time', round(time.perf_counter() - start, 4))
+    valid_tickets, error_rate = part1(rule_base, nearby_tickets)
+    print('part 1:', error_rate, 'time', round(time.perf_counter() - start, 4))
 
     start = time.perf_counter()
-    print('part 2:', part2(input_records), 'time', round(time.perf_counter() - start, 4))
+    print('part 2:', part2(rule_base, my_ticket, valid_tickets), 'time', round(time.perf_counter() - start, 4))
